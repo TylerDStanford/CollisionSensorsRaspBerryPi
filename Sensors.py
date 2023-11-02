22babd4e1a0329e5c513fa6230a469746928cbe8
Tyler Stanford <141964312+TylerDStanford@users.noreply.github.com>
2023-11-02 09:13:17 -0600

    Update Sensors.py

--- Sensors.py
+++ Sensors.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 from gpiozero import Buzzer #This is for the buzzer I metntioned in the read me. If you
 # dont want to add the buzzer ignore this and everything else that mentions buzzer.
 GPIO.setmode(GPIO.BMC)
-buzzer = Buzzer(18)
-GPIO.setup(24, GPIO.IN) #sets up the gpio pins the sensors will use.
+buzzer = Buzzer(18) #18 is the gpio pin that I use for the buzzer
+GPIO.setup(24, GPIO.IN) #sets up the gpio pins the sensors will use. (pin 24)
 xs = True
 while xs:
   if(GPIO.input(24)):# This says if there isn't any input from the sensor then print off and
